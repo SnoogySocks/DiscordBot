5a859b71ce286b8d4f08901fe52522b0b8f249ae
Felix <felixfong03@live.com>
2021-07-12 17:19:49 -0400

    made a src folder for better organization

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
 // Read in the commands and only use the ones ending in js
-const commandFiles = fs.readdirSync("./commands/").filter(file => file.endsWith(".js") && file !== "Command.js");
+const commandFiles = fs.readdirSync("./src/commands").filter(file => file.endsWith(".js") && file !== "Command.js");
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
